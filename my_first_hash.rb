57bae1423f076f69caa450cd965e20965bbfb477
Luis Herrera <herrera.lrh@gmail.com>
2019-10-29 00:27:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  your_hash = {"name" => "Luis"}
+  your_hash = {"chakra" => 100}
 end
 
 def shipping_manifest
@@ -7,25 +7,5 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-                 
   shipping_manifest["oil paintings"]
-end
-
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-
-  shipping_manifest["muskets"] = 2
-
-  shipping_manifest["gun powder"] = 4
-
-  shipping_manifest
 end
